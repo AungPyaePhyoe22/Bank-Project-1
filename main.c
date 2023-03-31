bcc0a1f29fa998aeea81a6e7ec2a77bae4a9c0ec
AungPyaePhyoe22 <123373421+AungPyaePhyoe22@users.noreply.github.com>
2023-03-31 22:22:35 +0630

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "test.h"
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+//    current_data_toTransfer(1000);
+//    unsigned int x = calculate_amounts_same_days(0);
+//    printf("%u",x);
+//    calculate_amounts_same_days(0);
+
+//    TIME();
+
+
+    return 0;
+}
